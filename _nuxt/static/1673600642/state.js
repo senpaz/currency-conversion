57ed683e44b8547dffca48f892de59efce592d19
Artem Nikolaev <senpaz07@gmail.com>
2023-01-13 14:11:31 +0500

    done

---
+++ _nuxt/static/1673600642/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcurrency-conversion\u002F_nuxt\u002Fstatic\u002F1673600642",layout:"default",error:a,state:{currencies:{currencies:[]}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fcurrency-conversion\u002F",assetsPath:"\u002Fcurrency-conversion\u002F_nuxt\u002F",cdnURL:a}}}}(null));
